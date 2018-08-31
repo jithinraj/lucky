49ff77b7edd20c5976de33429f151e5cfcc997c2
Paul Smith <paulcsmith0218@gmail.com>
2018-08-31 16:17:50 -0400

    Try to improve textconv

--- config/encrypted/encrypt-me.cr
+++
@@ -1,1 +0,0 @@
-"dOfGwmhLpMGKwpSZUdQaSW46gngbPTxPhPTNQmAdwATaNXOAcc4IhvnEXG8/+3beSO377twLoc/mzhsdwKkByQ==--d0MKaLrL+uDY+RPGl3MG+SulmCs="
--- tasks/enigma/textconv.cr
+++ tasks/enigma/textconv.cr
@@ -2,7 +2,20 @@ class Enigma::Textconv < LuckyCli::Task
   banner "Task used for textconv"
 
   def call
-    puts "ENIGMA textconv"
+    contents = STDIN.to_s
+    puts "CONTENTS"
+    puts contents
+    puts "FILENAME"
+    puts filename
+    contents = File.read(filename)
+    encryptor = Lucky::MessageEncryptor.new(secret: key)
+    puts encryptor.verify_and_decrypt(contents).to_s
+  end
+
+  @_key : String?
+
+  private def key : String
+    @_key ||= `git config #{Enigma::Setup::GIT_CONFIG_PATH_TO_KEY}`
   end
 
   private def run(command, io)
