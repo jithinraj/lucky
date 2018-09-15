a6051bed4be06134def61b92ef03190d8e008c32
Paul Smith <paulcsmith0218@gmail.com>
2018-09-15 13:05:49 -0400

    Testing it out

--- tasks/enigma/clean.cr
+++ tasks/enigma/clean.cr
@@ -1,9 +1,11 @@
 # https://www.juandebravo.com/2017/12/02/git-filter-smudge-and-clean
+# https://git-scm.com/book/en/v2/Customizing-Git-Git-Attributes
 class Enigma::Clean < LuckyCli::Task
   banner "Task used for git clean on checkin (file encryption)"
 
   def call
     contents = STDIN.gets.to_s.chomp
+    File.write "tmp/clean.log", "CLEANING!!!! at #{Time.now}, #{contents}"
     puts encrypt(contents)
   end
 
